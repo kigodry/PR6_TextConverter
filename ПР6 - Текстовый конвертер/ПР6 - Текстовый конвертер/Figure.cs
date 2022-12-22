dfc81be19fccc01ba5fe0d474c8e87fb94963fc3
Ivanov Zakhar <112906587+kigodry@users.noreply.github.com>
2022-12-23 00:01:17 +0300

    Add files via upload

---
+++ ПР6 - Текстовый конвертер/ПР6 - Текстовый конвертер/Figure.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Serialization;
+
+namespace ПР6___Текстовый_конвектор
+{
+    internal class Figure
+    {
+        public string name;
+        public string x;
+        public string y;
+        public static void txt_open(string path)
+        {
+
+            Text.txt = File.ReadAllText(path);
+            Text.txt1 = Text.text(path);
+            string a = string.Join("", Text.txt1);
+            Console.WriteLine(Text.txt);
+        }
+        public static void json_open(string path)
+        {
+
+            Text.txt = File.ReadAllText(path);
+            Text.txt1 = JsonConvert.DeserializeObject<List<Figure>>(Text.txt);
+            foreach (Figure i in Text.txt1)
+            {
+                Console.WriteLine(i.name);
+                Console.WriteLine(i.x);
+                Console.WriteLine(i.y);
+            }
+        }
+        public static void xml_open(string path)
+        {
+            // здесь открываем xlm`чик
+            XmlSerializer xml = new XmlSerializer(typeof(List<Figure>));
+            using FileStream fs = new FileStream(path, FileMode.Open);
+            Text.txt1 = (List<Figure>)xml.Deserialize(fs);
+            foreach (Figure i in Text.txt1)
+            {
+                Console.WriteLine(i.name);
+                Console.WriteLine(i.x);
+                Console.WriteLine(i.y);
+            }
+        }
+        public static void txt_save(string path)
+        {
+
+            string a = string.Join("", Text.txt1);
+            File.WriteAllText(path, "");
+            foreach (Figure i in Text.txt1)
+            {
+                File.AppendAllText(path, i.name + "\n");
+                File.AppendAllText(path, i.x + "\n");
+                File.AppendAllText(path, i.y + "\n");
+            }
+        }
+        public static void json_save(string path)
+        {
+
+            string json = JsonConvert.SerializeObject(Text.txt1);
+            string a = string.Join("", json);
+            File.WriteAllText(path, a);
+        }
+        public static void xml_save(string path)
+        {
+
+            XmlSerializer xml = new XmlSerializer(typeof(List<Figure>));
+            using FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+            xml.Serialize(fs, Text.txt1);
+        }
+    }
+}
---
+++ ПР6 - Текстовый конвертер/ПР6 - Текстовый конвертер/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ПР6___Текстовый_конвектор
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string path = menu1();
+                ConsoleKeyInfo key = menu2(path);
+                if (key.Key == ConsoleKey.Escape) { break; }
+                if (key.Key == ConsoleKey.F1)
+                {
+                    menu3();
+                }
+            }
+        }
+        static string menu1()
+        {
+
+            Console.WriteLine("===============================================");
+            Console.WriteLine("Введите путь до файла,который хотите открыть.");
+            Console.WriteLine("===============================================");
+            string path = Console.ReadLine();
+            Console.Clear();
+            return path;
+        }
+        static ConsoleKeyInfo menu2(string path)
+        {
+            ConsoleKeyInfo key;
+            while (true)
+            {
+                Console.WriteLine("=====================================================================");
+                Console.WriteLine("Сохранить файл в формате txt или json или xml - F1. Выход - Escape.");
+                Console.WriteLine("=====================================================================");
+                if (path.Contains(".txt")) { Figure.txt_open(path); }
+                if (path.Contains(".json")) { Figure.json_open(path); }
+                if (path.Contains(".xml")) { Figure.xml_open(path); }
+                key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape | key.Key == ConsoleKey.F1) { break; }
+                Console.Clear();
+            }
+            Console.Clear();
+            return key;
+        }
+        static void menu3()
+        {
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("Введите путь до файла, куда хотите сохранить текст.");
+            Console.WriteLine("=====================================================");
+            string path = Console.ReadLine();
+            if (path.Contains(".txt")) { Figure.txt_save(path); }
+            if (path.Contains(".json")) { Figure.json_save(path); }
+            if (path.Contains(".xml")) { Figure.xml_save(path); }
+            Console.Clear();
+        }
+    }
+}
